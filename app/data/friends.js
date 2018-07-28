f62361033140adac0c2a741d82faf4c201c5695d
Eric J <ejohnston092@gmail.com>
2018-07-27 23:53:50 -0400

    match not working...?

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,9 @@
 
 var friends = [
     {
-        "name": "Little Timmy",
-        "photo": "https://tinyurl.com/y9xpumwt",
-        "scores": [
+        name: "Little Timmy",
+        photo: "https://tinyurl.com/y9xpumwt",
+        scores: [
             5,
             1,
             4,
@@ -14,6 +14,51 @@ var friends = [
             5,
             4,
             1
+        ]
+    }, {
+        name: "Uther the Lightbringer",
+        photo: "https://tinyurl.com/ycm8xh6z",
+        scores: [
+            1,
+            4,
+            2,
+            4,
+            5,
+            1,
+            5,
+            4,
+            3,
+            1
+        ]
+    }, {
+        name: "Bill S. Preston",
+        photo: "https://tinyurl.com/ydfz8zwb",
+        scores: [
+            3,
+            4,
+            1,
+            2,
+            5,
+            4,
+            3,
+            2,
+            1,
+            4
+        ]
+    }, {
+        name: "Theodore Logan",
+        photo: "https://tinyurl.com/y8sms5bd",
+        scores: [
+            4,
+            3,
+            1,
+            5,
+            2,
+            3,
+            2,
+            1,
+            4,
+            3
         ]
     }
 ];
